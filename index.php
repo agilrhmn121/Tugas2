0058c590de53446f4e2a65abb3f3724d20a049bf
agilrhmn121 <akuagil123@gmail.com>
2025-03-11 22:48:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,11 @@
 <?php
 include "Mahasiswa.php";
-
-// Objek pertama dengan data mahasiswa Anda
-$mahasiswaTI = new Mahasiswa();
-$mahasiswaTI->setData('A11.2023.15406', 'Agil Rahman', 'Teknik Informatika');
-print_r($mahasiswaTI->getData());
-
-// Objek kedua (baru)
-$mahasiswaSI = new Mahasiswa();
-$mahasiswaSI->setData('A11.2000.00001', 'Fulan', 'Teknik Informasi');
-print_r($mahasiswaSI->getData());
-
-// Objek ketiga (baru)
-$mahasiswaDKV = new Mahasiswa();
-$mahasiswaDKV->setData('A12.2000.00001', 'Junior', 'Desain Komunikasi Visual');
-print_r($mahasiswaDKV->getData());
-
-// Menampilkan data mahasiswa dengan method tampilData()
-echo "<br>Data Mahasiswa dengan method tampilData():<br>";
-$mahasiswaTI->tampilData();
+$mahasiswa1 = new Mahasiswa();
+$mahasiswa2 = new Mahasiswa();
+$mahasiswa1->setData('A11.2023.15406', 'Agil Rahman', 'Teknik Informatika');
+$mahasiswa2->setData('A11.2023.15444', 'Furina', 'Teknik Informatika');
+echo "Data Mahasiswa : <br>";
+$mahasiswa1->TampilData();
 echo "<br>";
-$mahasiswaSI->tampilData();
-echo "<br>";
-$mahasiswaDKV->tampilData();
-?>
+$mahasiswa2->TampilData();
+?>  
